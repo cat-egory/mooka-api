4494d55a87f6f26004271fa4290980d0059a0d26
cat-egory <xtheblackeyes@gmail.com>
2021-03-28 14:17:41 +0900

    프로젝트 설정 잘못해 만들어서 3번째 생성

--- src/main/java/xyz/mooka/api/ApiApplication.java
+++ src/main/java/xyz/mooka/api/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MookaApiApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MookaApiApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
