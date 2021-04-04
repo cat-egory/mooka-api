03e6c3ed865ded06c4658f6fc0fb8561961be7d7
cat-egory <xtheblackeyes@gmail.com>
2021-04-04 13:07:20 +0900

    feat: git commit 실수.
    빠진 naverGateway 추가

---
+++ src/main/java/xyz/mooka/api/gateway/NaverGateway.java
@@ -0,0 +1,50 @@
+package xyz.mooka.api.gateway;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class NaverGateway {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static WebClient webClient;
+
+    @Value("${api.naver.clientId}")
+    String naverClientId;
+
+    @Value("${api.naver.naverSecret}")
+    String naverSecret;
+
+    @Autowired
+    NaverGateway() {
+        webClient =
+            WebClient
+                .builder()
+                .baseUrl("https://openapi.naver.com")
+                .defaultHeader("X-Naver-Client-Id", naverClientId)
+                .defaultHeader("X-Naver-Client-Secret", naverSecret)
+                .build();
+    }
+
+    public String get(String url) {
+        Mono<String> stringMono = webClient.mutate().build().get().uri(url).retrieve().bodyToMono(String.class);
+        Optional<String> stringOptional = stringMono.blockOptional();
+        if (stringOptional.isPresent()) {
+            return stringOptional.get();
+        }
+
+        return "";
+    }
+
+    public void showNaverInfo() {
+        logger.info(naverClientId);
+    }
+}
